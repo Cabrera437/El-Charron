93922fce098f7a84c4d97a1ac807f722f39d408e
Murcia <nelsonricardomurcia2001@gmail.com>
2025-11-05 01:20:22 -0600

    integrando firebase

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,10 @@
+/*buildscript {
+    dependencies {
+        // Fíjate: son paréntesis y comillas dobles
+        classpath("com.google.gms:google-services:4.4.1") 
+    }
+}*/
+
 allprojects {
     repositories {
         google()
--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,16 @@ import 'package:el_charron/screens/CrearProducto.dart' as crear;
 import 'package:el_charron/screens/AnadirProducto.dart';
 import 'package:flutter/material.dart';
 import 'screens/CrearSesion.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() => runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
